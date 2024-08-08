906bee9e3b1f6956aaeee97262e141e96b4a550d
sonpnph <sonpnph37079@fpt.edu.vn>
2024-08-08 23:52:20 +0700

    FE/feature-redux

--- teddy_fe/src/app/store.ts
+++ teddy_fe/src/app/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authSlice from '~/feature/auth/authSlice'
+import blogSlice from '~/feature/blog/blogSlice'
 import productSlice from '~/feature/product/productSlice'
 
 export const store = configureStore({
   reducer: {
     auth: authSlice,
-    product: productSlice
+    product: productSlice,
+    blog: blogSlice
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
--- teddy_fe/src/pages/Blogs/Blogs.tsx
+++ teddy_fe/src/pages/Blogs/Blogs.tsx
@@ -5,10 +5,38 @@ import Heading from '~/components/Heading'
 import Button from '~/components/Button'
 import images from '~/assets'
 import BlogList from '~/components/BlogList'
+import { useDispatch, useSelector } from 'react-redux'
+import { useEffect, useState } from 'react'
+import { getBlogByCategory, getBlogs, getCategories } from '~/features/blog/blogSlice'
+import { blogCategory } from '~/models'
 
 const cx = classNames.bind(styles)
-const categories = ['Xem tất cả', 'Công nghệ', 'Kiến thức & Phần mềm', 'Sử dụng và bảo dưỡng', 'Tin tức và xu hướng']
+
 const Blogs = () => {
+  const dispatch = useDispatch()
+  const [blogId, setBlogId] = useState<string>('')
+
+  useEffect(() => {
+    if (blogId !== '') dispatch<any>(getBlogByCategory(blogId))
+    else dispatch<any>(getBlogs())
+  }, [dispatch, blogId])
+
+  useEffect(() => {
+    dispatch<any>(getCategories())
+    dispatch<any>(getBlogs())
+  }, [dispatch])
+
+  const blogState = useSelector((state: any) => state.blog)
+  const { blogCategories, blogs } = blogState
+
+  const categories = blogCategories?.map((blogCat: blogCategory) => ({
+    name: blogCat.name,
+    _id: blogCat._id
+  }))
+
+  const onBlogChange = (id: string) => {
+    setBlogId(id)
+  }
   return (
     <div className={cx('blog-wrapper')}>
       <section className={cx('blog-section')}>
@@ -24,7 +52,7 @@ const Blogs = () => {
           heading="Khám phá bài viết mới nhất của chúng tôi"
           desc="Cập nhật thông tin với các bài viết mới nhất của chúng tôi."
         />
-        <BlogList limit={3} isShowMore />
+<BlogList limit={6} isShowCategory categories={categories} data={blogs} onBlogChange={onBlogChange} />
       </section>
 
       <section className={cx('blog-newsletter')}>
@@ -33,6 +61,7 @@ const Blogs = () => {
             heading="Theo dõi bản tin của chúng tôi"
             desc="Luôn cập nhật các bài viết và tin tức mới nhất về bàn phím trên thị trường."
           />
+          <BlogList isShowMore data={blogs} limit={3} />
           <div className={cx('newsletter-action')}>
             <input type="email" placeholder="Nhập email của bạn" />
             <Button small background>
