9a1f35dcc05df2586413a026d27a59d597ebdfe9
MAiDOANHIEU <hieumdph36863@fpt.edu.vn>
2024-08-09 16:37:34 +0700

    FE/feat/user-profile-ui

--- teddy_fe/src/App.tsx
+++ teddy_fe/src/App.tsx
@@ -1,5 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { publicRoutes } from '~/routes'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { privateRoutes, publicRoutes } from '~/routes'
 import { route } from './models'
 import MainLayout from '~/layouts/MainLayout'
 import { useEffect } from 'react'
@@ -10,6 +10,7 @@ function App() {
       behavior: 'smooth'
     })
   }, [])
+  const isLoggedIn = getAccessTokenFromLocalStorage()
   return (
     <Router>
       <div className="App">
@@ -30,6 +31,30 @@ function App() {
                   <Layout>
                     <Page />
                   </Layout>
+                }
+              />
+            )
+          })}
+          {privateRoutes.map((route: route, index: number) => {
+            let Layout: React.ComponentType<any> = MainLayout
+            if (route.layout) {
+              Layout = route.layout as React.ComponentType<any>
+            }
+
+            const Page = route.element
+
+            return (
+              <Route
+                key={index}
+                path={route.path}
+                element={
+                  isLoggedIn ? (
+                    <Layout>
+                      <Page />
+                    </Layout>
+                  ) : (
+                    <Navigate to="/login" replace state={{ previousPath: route.path }} />
+                  )
                 }
               />
             )
